ee29bf2e01bee27020713371b332688d36d81925
louisohlow <l.ohlow@sevensenders.com>
2021-07-12 15:52:28 +0200

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import type { Node } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+        <View>
+          <Text>Hallo</Text>
+        </View>
+  );
+};
+
+export default App;
