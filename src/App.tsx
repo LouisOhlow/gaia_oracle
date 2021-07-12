7bc945737e90e5e98bf0d5798a6162bc248f390d
louisohlow <l.ohlow@sevensenders.com>
2021-07-12 16:22:43 +0200

    added basic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import type { Node } from 'react';
 import {
-  Text,
+  TouchableOpacity,
   View,
+  StyleSheet,
+  Text,
 } from 'react-native';
 
-const App: () => Node = () => {
-  return (
-        <View>
-          <Text>Hallo</Text>
-        </View>
-  );
-};
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
 
+interface State {
+  enthusiasmLevel: number;
+}
+
+export class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <Text style={styles.buttonTitle}> TELL ME THE FUTURE </Text>
+          </TouchableOpacity>
+      </View>
+      );
+    }
+  };
 export default App;
+
+const styles = StyleSheet.create({
+  button: {
+    height: '8%',
+    alignSelf: 'center',
+    width: '50%',
+    borderRadius: 50,
+    borderColor: 'black',
+    borderWidth: 5,
+    top: 700
+  },
+  container: {
+    width: '100%',
+    height: '100%',
+    },
+  buttonTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  }
+})
